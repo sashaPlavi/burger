757b3d7b5e082bb51ed8aaeb9ba9b3ec791d57a8
SashaPlavi <sasha.bingulac@gmail.com>
2019-12-10 16:46:44 +0100

    stop

--- src/components/burger/ingridient/ingridient.js
+++ src/components/burger/ingridient/ingridient.js
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Ingridient = props => {
-  let ingridient = null;
+class Ingridient extends Component {
+  render() {
+    let ingridient = null;
 
-  switch (props.type) {
-    case "bread-botom":
-      ingridient = <div className="BreadBotom"></div>;
-      break;
-    case "bread-top":
-      ingridient = (
-        <div className="BreadTop">
-          <div className="Seed1"></div>
-          <div className="Seed1"></div>
-        </div>
-      );
-      break;
+    switch (this.props.type) {
+      case "bread-botom":
+        ingridient = <div className="BreadBotom"></div>;
+        break;
+      case "bread-top":
+        ingridient = (
+          <div className="BreadTop">
+            <div className="Seed1"></div>
+            <div className="Seed1"></div>
+          </div>
+        );
+        break;
 
-    case "meat":
-      ingridient = <div className="Meat"></div>;
-      break;
-    case "cheese":
-      ingridient = <div className="Cheese"></div>;
-      break;
-    case "bacon":
-      ingridient = <div className="Bacon"></div>;
-      break;
-    case "salad":
-      ingridient = <div className="Salad"></div>;
-      break;
-    default:
-      ingridient = null;
+      case "meat":
+        ingridient = <div className="Meat"></div>;
+        break;
+      case "cheese":
+        ingridient = <div className="Cheese"></div>;
+        break;
+      case "bacon":
+        ingridient = <div className="Bacon"></div>;
+        break;
+      case "salad":
+        ingridient = <div className="Salad"></div>;
+        break;
+      default:
+        ingridient = null;
+    }
+    return ingridient;
   }
-  return ingridient;
-};
+}
 
 export default Ingridient;
